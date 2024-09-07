796a65ad65f61c628c73c736f4b969dabb531449
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-09-07 23:40:06 +0100

    feat: update icons, update hero, update header, update color, update container

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rGG0r8cLmfeIwR5fpfwTt0SZZ9jetvNgmVY7LAFaeJU=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import Header from "@/components/layouts/header/Header";
+import Logo from "../components/elements/icons/Logo";
+
+export default function Home() {
+  return <main className="no-scrollbar"></main>;
+}
